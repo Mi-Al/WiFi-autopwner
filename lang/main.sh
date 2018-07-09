80913e5c052bd3c1a40f1d81f61e8377a37d2c7f
Onefivefournine <onefivefournine@gmail.com>
2018-07-09 10:12:52 +0300

    fixes

--- lang/main.sh
+++ lang/main.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 declare -A Lang
-echo "Lang file is: $(dirname $0)/lang/$LANGUAGE.txt"
 while read line; do
-    key=$(echo $line | grep -Po "^(.*)(?=\:)")
-    text=$(echo $line | grep -Po "^.*:\K(.*)$")
+    key=$(echo $line | grep -Po "^(.*)(?=::.*$)")
+    text=$(echo $line | grep -Po "^.*::\K(.*)$")
     Lang[$key]=$text
 done <$(dirname $0)/lang/$LANGUAGE.txt
